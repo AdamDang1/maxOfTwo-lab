7c65f06fdf6344fafb59041906cb634834a4120b
AdamDang1 <dtdang1@gmail.com>
2022-02-25 12:15:03 -0600

    maxOfTwo challenge completed/forgot inital commit

---
+++ maxOfTwo.js
@@ -0,0 +1,11 @@
+let x = 6
+let y = 6
+
+// Write your code below this line.
+if (x > y) {
+    console.log(x);
+} else if (y > x) {
+    console.log(y);
+} else {
+    console.log("They have the same value");
+}
